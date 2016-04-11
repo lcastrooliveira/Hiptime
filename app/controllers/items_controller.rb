a901b032bbde0cf4202fdc57473d2016070d7cb7
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-04-11 16:58:16 -0300

    Add Edit and Destroy Actions and View Files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
   def show
 
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
